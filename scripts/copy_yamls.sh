d73e8d3a4bf199869b25271714fd42963da1755a
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 10:13:34 +0300

    added audible data

---
+++ scripts/copy_yamls.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eu
+
+# copy yaml files from the [data] repo to the right place in this one
+cp -f "../data/yaml/"{audio_courses.yaml,video_series.yaml,audible.yaml} "docs/data"
--- scripts/open_local_browser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-
-xdg-open "https://veltzer.github.io"
