e4946e9afea1f1347938e152d4b57ffbb30789b7
Mark Veltzer <mark.veltzer@gmail.com>
2026-02-12 17:40:35 +0200

    no commit message given

--- scripts/copy_yamls.sh
+++ scripts/copy_yamls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
 # copy yaml files from the [data] repo to the right place in this one
-cp -f "../data/yaml/"{museums.yaml,video_features.yaml,audio_courses.yaml,video_series.yaml,audible.yaml} "docs/data"
+cp -f "../data/yaml/"{podcasts.yaml,museums.yaml,video_features.yaml,audio_courses.yaml,video_series.yaml,audible.yaml} "docs/data"
 cp -f "../data/raw/topics/games/chess/games.pgn" "docs/data"
